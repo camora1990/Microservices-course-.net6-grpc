6550d3f931362c9792405b167512300559c1a7e1
camora1990 <moorales.camilo@gmail.com>
2023-07-24 08:55:55 -0500

    Platform repository

---
+++ PlatfomServices/src/Domain/Domain.Model/Repositories/IPlatformRespository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Model.Models;
+
+namespace Domain.Model.Repositories;
+
+public interface IPlatformRespository
+{
+    Task<IEnumerable<Platform>> GetAllAsync();
+
+    Task<Platform> GetByIdAsync(long id);
+
+    Task<Platform> CreateAsync(Platform platform);
+}
