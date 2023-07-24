7b0481a053813791f1d379a9aea8166552c35102
camora1990 <moorales.camilo@gmail.com>
2023-07-24 08:56:58 -0500

    dependency injection use case

---
+++ PlatfomServices/src/Domain/Domain.UseCase/UseCaseExtensions/UseCaseExtension.cs
@@ -0,0 +1,14 @@
+﻿using Domain.UseCase.Interfaces;
+using Domain.UseCase.UseCases;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Domain.UseCase.UseCaseExtensions;
+
+public static class UseCaseExtension
+{
+    public static IServiceCollection AddServicesUseCase(this IServiceCollection services)
+    {
+        services.AddTransient<IPlatformUseCase, PlatformUseCase>();
+        return services;
+    }
+}
