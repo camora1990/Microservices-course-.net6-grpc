68b62d4e68b00229df7119be11828d3925e6c671
camora1990 <moorales.camilo@gmail.com>
2023-07-24 10:09:29 -0500

    controller base

---
+++ PlatfomServices/src/Infrastructure/EntryPoints/EntryPoint.Web/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EntryPoint.Web.Controllers;
+
+[ApiController]
+[Route("api/v1/[Controller]/[Action]")]
+public class BaseController : ControllerBase
+{
+    public async Task<IActionResult> HandleRequest<TResult>(Func<Task<TResult>> requestHandler, string message)
+    {
+        var resp = await requestHandler();
+        try
+        {
+            return Ok(new { message, ok = true, data = resp });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { message = e.Message, ok = false });
+        }
+    }
+}
---
+++ PlatfomServices/src/Infrastructure/EntryPoints/EntryPoint.Web/Controllers/PlatformController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Domain.UseCase.Interfaces;
+using EntryPoint.Web.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntryPoint.Web.Controllers;
+
+public class PlatformController : BaseController
+{
+    private readonly IMapper _mapper;
+    private readonly IPlatformUseCase _useCase;
+
+    public PlatformController(IMapper mapper, IPlatformUseCase useCase)
+    {
+        _mapper = mapper;
+        _useCase = useCase;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+        => await HandleRequest(async () =>
+             _mapper.Map<IEnumerable<PlatformDto>>(await _useCase.GetAll())
+       , "List platform");
+}
