5e472b325d361943fb7cf3066a6cb83137cb7438
camora1990 <moorales.camilo@gmail.com>
2023-07-24 08:56:12 -0500

    Platform use case

---
+++ PlatfomServices/src/Domain/Domain.UseCase/Interfaces/IPlatformUseCase.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Model.Models;
+
+namespace Domain.UseCase.Interfaces;
+
+public interface IPlatformUseCase
+{
+    Task<IEnumerable<Platform>> GetAll();
+
+    Task<Platform> GetById(long id);
+
+    Task<Platform> Create(Platform platform);
+}
---
+++ PlatfomServices/src/Domain/Domain.UseCase/UseCases/PlatformUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Model.Models;
+using Domain.Model.Repositories;
+using Domain.UseCase.Interfaces;
+
+namespace Domain.UseCase.UseCases;
+
+public class PlatformUseCase : IPlatformUseCase
+{
+    private readonly IPlatformRespository _respository;
+
+    public PlatformUseCase(IPlatformRespository respository)
+    {
+        _respository = respository;
+    }
+
+    public async Task<Platform> Create(Platform platform)
+    {
+        return await _respository.CreateAsync(platform);
+    }
+
+    public async Task<IEnumerable<Platform>> GetAll()
+    {
+        return await _respository.GetAllAsync();
+    }
+
+    public async Task<Platform> GetById(long id)
+    {
+        return await _respository.GetByIdAsync(id);
+    }
+}
