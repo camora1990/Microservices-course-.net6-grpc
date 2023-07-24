1cdc9a53923f41346b29da95dacbdb402c90b986
camora1990 <moorales.camilo@gmail.com>
2023-07-24 10:09:14 -0500

    dtos platform

---
+++ PlatfomServices/src/Infrastructure/EntryPoints/EntryPoint.Web/Dtos/CreatePlatformDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EntryPoint.Web.Dtos;
+
+public class CreatePlatformDto
+{
+    public string Name { get; set; }
+    public string Publisher { get; set; }
+    public string Cost { get; set; }
+}
---
+++ PlatfomServices/src/Infrastructure/EntryPoints/EntryPoint.Web/Dtos/PlatformDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EntryPoint.Web.Dtos
+{
+    public class PlatformDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Cost { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
